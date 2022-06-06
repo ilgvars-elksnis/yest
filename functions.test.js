727a464e843942c768842450a6656b9d64a5a8a5
ilgvars-elksnis <69342948+ilgvars-elksnis@users.noreply.github.com>
2022-06-06 08:41:02 +0300

    Add files via upload

---
+++ functions.test.js
@@ -0,0 +1,54 @@
+const functions = require('./functions');
+
+test('Adds 2 + 2 to equal 4', () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test('Adds 2 + 2 to NOT equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// toBeNull
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue({})).toBeFalsy();
+});
+
+// toEqual
+test('User should be Ilgvars Elksnis object', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Ilgvars',
+    lastName: 'Elksnis'
+  });
+});
+
+// Less than and greater than
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/i);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
+});
+
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
+});
